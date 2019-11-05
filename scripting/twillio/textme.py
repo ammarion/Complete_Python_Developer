c2e8234269e024d912a0db44b996432290c44ed8
Ammar Alim <ammar.alim@nordstrom.com>
2019-11-05 09:24:22 -0800

    mass upload

--- scripting/twillio/textme.py
+++ scripting/twillio/textme.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = "AC501daf587f0fe8cd51e8c03b52247b45"
-auth_token = "b2be2f8c1104350b2d51933ec98cb54a"
+account_sid = "AC50"
+auth_token = "b3ecb54a"
 
 client = Client(account_sid, auth_token)
 message = client.messages.create(
